301f3bc9edd7f6f2729fa84ed0064a92af4f25c4
hermannozor <hermannfotso2@gmail.com>
2017-12-03 08:56:00 +0100

    First Commit of the Project
    Premier Commit de mon projet.

---
+++ Assets/checkWeapon.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkWeapon : MonoBehaviour {
+
+   
+    private int id_Arme;
+
+  
+    public List<GameObject> list_Arme = new List<GameObject>();
+
+    [SerializeField]
+
+	void Start () {
+
+       
+
+    }
+	
+	
+	void Update () {
+
+        if(transform.childCount > 0)
+        {
+            id_Arme = gameObject.GetComponentInChildren<ItemOnObject>().item.itemID;
+        }
+        else
+        {
+            id_Arme = 0;
+
+            for (int i = 0; i < list_Arme.Count; i++)
+            {
+
+                list_Arme[i].SetActive(false);
+            }
+        }
+
+        if (id_Arme == 1 && transform.childCount > 0)
+        {
+            for (int i = 0; i < list_Arme.Count; i++)
+            {
+                if (i == 0)
+                {
+                    list_Arme[i].SetActive(true);
+                }
+            }
+
+        }
+
+        if (id_Arme == 2 && transform.childCount > 0)
+        {
+            for (int i = 1; i < list_Arme.Count; i++)
+            {
+                if (i == 1)
+                {
+                    list_Arme[i].SetActive(true);
+                }
+            }
+
+        }
+
+    }
+}
